8084168c00b4e36596827b004b946a6a380ac86d
Christopher Ochoa <christopher.ochoa@globant.com>
2023-12-21 23:53:03 -0500

    Created: SignUpScreen class

---
+++ src/test/java/com/mobileAutomationFinalPractice/Screens/SignUpScreen.java
@@ -0,0 +1,10 @@
+package com.mobileAutomationFinalPractice.Screens;
+
+import com.mobileAutomationFinalPractice.utils.screens.BaseScreen;
+import io.appium.java_client.android.AndroidDriver;
+
+public class SignUpScreen extends BaseScreen {
+    public SignUpScreen(AndroidDriver driver) {
+        super(driver);
+    }
+}
